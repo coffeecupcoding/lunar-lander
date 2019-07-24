113bcca0bb3ac8cb4400c4d96889f8ee49b8ebdd
James Hewitt <jgh@caurinus.com>
2019-07-23 22:20:43 -0700

    Added Kinematics class and integrated
    Kinematics replaces separate tracking of position, velocity, and time
    The global variable elapsed_time is now in a Kinematics instance that is
    part of the Lander instance

--- lunar-lander.py
+++ lunar-lander.py
@@ -9,56 +9,22 @@ import math
 import sys
 
 # Globals
-elapsed_time = 0
 gravity = 0.001
+
 
 class GameOver(Exception): pass
 
-class lander:
+
+class Kinematics:
     """
-    Lander data and status
+    Kinematics-related data
     """
-    def __init__(self, initial_altitude=120.0, initial_velocity=1.0,
-            capsule_mass=16500.0, initial_fuel=16500.0,
-            fuel_specific_impulse = 1.8):
-         self.altitude = initial_altitude
-         self.velocity = initial_velocity
-         self.capsule_mass = capsule_mass
-         self.fuel = initial_fuel  # in pounds, um, mass
-         self.total_mass = self.capsule_mass + self.fuel
-         self.fuel_isp = fuel_specific_impulse
+    def __init__(self, altitude, velocity, time=0.0):
+        self.altitude = altitude
+        self.velocity = velocity
+        self.elapsed_time = time
 
-    def out_of_fuel(self):
-        if self.fuel < 0.001:
-            return True
-        return False
-
-    def calc_mass_change(self, burn_rate, burn_time):
-        # returns proportional change in mass due to fuel usage
-        return (burn_rate * burn_time) / self.total_mass
-
-    def velocity_mass_factor(self, q):
-        """
-        Effect on velocity from integrated change of mass
-        q is the proportional change in mass due to fuel usage
-        """
-        factor = ((-q) +
-                 (-1 * ((q ** 2) / 2.0)) +
-                 (-1 * ((q ** 3) / 3.0)) +
-                 (-1 * ((q ** 4) / 4.0)) +
-                 (-1 * ((q ** 5) / 5.0)))
-        return factor
-
-    def calc_velocity(self, burn_rate, burn_time, q):
-        """
-        Caculate new velocity based on starting velocity and delta t
-        """
-        velocity = (self.velocity + 
-                   (gravity * burn_time) +
-                   (self.fuel_isp * self.velocity_mass_factor(q)))
-        return velocity
-
-    def altitude_mass_factor(self, q):
+    def altitude_mass_factor(q):
         """
         Effect on altitude from integrated change of mass
         q is the proportional change in mass due to fuel usage
@@ -70,25 +36,70 @@ class lander:
                  ((q ** 5) / 30.0))
         return factor
 
+    def velocity_mass_factor(q):
+        """
+        Effect on velocity from integrated change of mass
+        q is the proportional change in mass due to fuel usage
+        """
+        factor = ((-q) +
+                 (-1 * ((q ** 2) / 2.0)) +
+                 (-1 * ((q ** 3) / 3.0)) +
+                 (-1 * ((q ** 4) / 4.0)) +
+                 (-1 * ((q ** 5) / 5.0)))
+        return factor
+
+
+class Lander:
+    """
+    Lander data and status
+    """
+    def __init__(self, initial_altitude=120.0, initial_velocity=1.0,
+            capsule_mass=16500.0, initial_fuel=16500.0,
+            fuel_specific_impulse = 1.8):
+         self.capsule_mass = capsule_mass
+         self.fuel = initial_fuel  # in pounds, um, mass
+         self.total_mass = self.capsule_mass + self.fuel
+         self.fuel_isp = fuel_specific_impulse
+         self.phys = Kinematics(initial_altitude, initial_velocity)
+
+    def out_of_fuel(self):
+        if self.fuel < 0.001:
+            return True
+        return False
+
+    def calc_mass_change(self, burn_rate, burn_time):
+        # returns proportional change in mass due to fuel usage
+        return (burn_rate * burn_time) / self.total_mass
+
+    def calc_velocity(self, burn_rate, burn_time, q):
+        """
+        Caculate new velocity based on starting velocity and delta t
+        """
+        velocity = (self.phys.velocity +
+                   (gravity * burn_time) +
+                   (self.fuel_isp * Kinematics.velocity_mass_factor(q)))
+        return velocity
+
     def calc_altitude(self, burn_rate, burn_time, q):
         """
         Caculate new altitude based on starting altitude and delta t
         """
-        altitude = (self.altitude +
+        altitude = (self.phys.altitude +
                    ((-1) * (gravity * ((burn_time ** 2) / 2))) +
-                   ((-1) * self.velocity * burn_time) +
-                   (self.fuel_isp * burn_time * self.altitude_mass_factor(q)))
+                   ((-1) * self.phys.velocity * burn_time) +
+                   (self.fuel_isp * burn_time *
+                       Kinematics.altitude_mass_factor(q)))
         return altitude
 
     def calc_dynamics(self, burn_rate, burn_time):
         """
         Return new altitude and velocity based on burn rate and time
-        Returns the values as a tuple
+        Returns the values as a Kinematics instance
         """
         q = self.calc_mass_change(burn_rate, burn_time)
         velocity = self.calc_velocity(burn_rate, burn_time, q)
         altitude = self.calc_altitude(burn_rate, burn_time, q)
-        return (altitude, velocity)
+        return Kinematics(altitude, velocity)
 
     def calc_burn_time(self, burn_rate, time_left):
         # Reduce burn time if there isn't enough fuel
@@ -105,38 +116,37 @@ class lander:
             (self.total_mass * gravity) / (self.fuel_isp * burn_rate)) / 2
         # the units work at least...
         # the '+ 0.05' at the end is for ?? good luck?
-        new_burn_time = ((self.total_mass * self.velocity) /
+        new_burn_time = ((self.total_mass * self.phys.velocity) /
             (self.fuel_isp * burn_rate * (factor + math.sqrt(
-            (factor * factor) + (self.velocity / self.fuel_isp))))) + 0.05
+            (factor * factor) + (self.phys.velocity / self.fuel_isp))))) + 0.05
         return new_burn_time
 
-    def update_status(self, burn_rate, burn_time, altitude, velocity, time):
+    def update_status(self, burn_rate, burn_time, new_phys, time_left):
         """
         Set lander data and elapsed time
         Returns the remaining time in this period
         """
-        global elapsed_time
-        elapsed_time = elapsed_time + burn_time
-        self.altitude = altitude
-        self.velocity = velocity
+        self.phys.elapsed_time = self.phys.elapsed_time + burn_time
+        self.phys.altitude = new_phys.altitude
+        self.phys.velocity = new_phys.velocity
         self.fuel = self.fuel - (burn_rate * burn_time)
         self.total_mass = self.capsule_mass + self.fuel
-        return (time - burn_time)
+        return (time_left - burn_time)
 
     def calc_impact(self, burn_rate, iter_time):
         """
         Determine 'landing' parameters
         """
         while iter_time >= 0.005:
-            iter_velocity = (self.velocity +
-                math.sqrt((self.velocity ** 2) +
-                (2 * self.altitude * ((gravity) - 
+            iter_velocity = (self.phys.velocity +
+                math.sqrt((self.phys.velocity ** 2) +
+                (2 * self.phys.altitude * ((gravity) -
                 (self.fuel_isp * (burn_rate / self.total_mass))))))
-            iter_time = 2 * (self.altitude / iter_velocity)
-            (next_alt, next_vel) = self.calc_dynamics(burn_rate, iter_time)
+            iter_time = 2 * (self.phys.altitude / iter_velocity)
+            new_phys = self.calc_dynamics(burn_rate, iter_time)
             # the return value is intentionally ignored here
             # because the end is near
-            self.update_status(burn_rate, iter_time, next_alt, next_vel, 0)
+            self.update_status(burn_rate, iter_time, new_phys, 0)
 
 
 
@@ -148,19 +158,18 @@ def out_of_fuel(lem):
     """
     Print message and set final velocity and time
     """
-    global elapsed_time
-    print("\nFUEL OUT AT %d SECONDS" % elapsed_time)
-    seconds_to_impact = (((-1.0) * lem.velocity) +
-        math.sqrt((lem.velocity * lem.velocity) +
-            (2.0 * lem.altitude * gravity))) / gravity
-    lem.velocity = lem.velocity + (gravity * seconds_to_impact)
-    elapsed_time = elapsed_time + seconds_to_impact
+    print("\nFUEL OUT AT %d SECONDS" % lem.phys.elapsed_time)
+    seconds_to_impact = (((-1.0) * lem.phys.velocity) +
+        math.sqrt((lem.phys.velocity * lem.phys.velocity) +
+            (2.0 * lem.phys.altitude * gravity))) / gravity
+    lem.phys.velocity = lem.phys.velocity + (gravity * seconds_to_impact)
+    lem.phys.elapsed_time = lem.phys.elapsed_time + seconds_to_impact
     end_game(lem)
 
 def end_game(lem):
-    velocity_mph = lem.velocity * 3600.0
+    velocity_mph = lem.phys.velocity * 3600.0
     print("\nON THE MOON AT %d SECONDS\nIMPACT VELOCITY %d MPH\n" % 
-        (elapsed_time, velocity_mph))
+        (lem.phys.elapsed_time, velocity_mph))
     if velocity_mph < 1.2:
         print("PERFECT LANDING!!")
     elif velocity_mph <= 10.0:
@@ -191,11 +200,11 @@ def output_header():
     print(" SEC  MILES  FEET    MPH   FUEL  RATE")
 
 def output_status(lem):
-    miles = int(lem.altitude)
-    feet = 5280 * (lem.altitude - miles)
-    mph = 3600 * lem.velocity
+    miles = int(lem.phys.altitude)
+    feet = 5280 * (lem.phys.altitude - miles)
+    mph = 3600 * lem.phys.velocity
     print("%4d    %3d  %4d  %5d  %5d" %
-        (elapsed_time, miles, feet, mph, lem.fuel), end='  ')
+        (lem.phys.elapsed_time, miles, feet, mph, lem.fuel), end='  ')
 
 def get_burn_rate():
     user_input = ""
@@ -224,10 +233,8 @@ def run_game():
         "INITIAL FUEL IS 16,500 LBS.\n\n\n"
         "GOOD LUCK!\n\n"
     )
-    global elapsed_time
     
-    elapsed_time = 0.0
-    lem = lander()
+    lem = Lander()
     output_header()
     while True:
         burn_rate = 0.0
@@ -241,17 +248,17 @@ def run_game():
                 break
             burn_time = lem.calc_burn_time(burn_rate, this_period)
             lem.calc_dynamics(burn_rate, burn_time)
-            (new_alt, new_vel) = lem.calc_dynamics(burn_rate, burn_time)
-            if new_alt <= 0:
+            new_phys = lem.calc_dynamics(burn_rate, burn_time)
+            if new_phys.altitude <= 0:
                 landing(lem, burn_rate, burn_time)
-            if lem.velocity > 0:
-                if new_vel < 0:
+            if lem.phys.velocity > 0:
+                if new_phys.velocity < 0:
                     burn_time = lem.calc_upward_burn_time(burn_rate)
-                    (new_alt, new_vel) = lem.calc_dynamics(burn_rate, burn_time)
-                    if new_alt <= 0:
+                    new_phys = lem.calc_dynamics(burn_rate, burn_time)
+                    if new_phys.altitude <= 0:
                         landing(lem, burn_rate, burn_time)
             this_period = lem.update_status(burn_rate, burn_time,
-                              new_alt, new_vel, this_period)
+                              new_phys, this_period)
 
 def run():
     intro()
